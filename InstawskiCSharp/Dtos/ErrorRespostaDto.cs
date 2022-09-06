a195f05c9eca7b4fdf8462a2e69d8b3c15ea8a84
Ariel Araujo <a.lucasdearaujo@outlook.com>
2022-09-05 21:43:35 -0300

    Aula 14 realizando a implementação do Login

---
+++ InstawskiCSharp/Dtos/ErrorRespostaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace InstawskiCSharp.Dtos
+{
+    public class ErrorRespostaDto
+    {
+
+        public int Status { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ InstawskiCSharp/Dtos/LoginRequisicaoDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace InstawskiCSharp.Dtos
+{
+    public class LoginRequisicaoDto
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
